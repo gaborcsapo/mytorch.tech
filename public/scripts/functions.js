c7d0156b2f124afb382f466f0323e26c64861f2e
jihjihk <jihyun@nyu.edu>
2017-10-17 15:29:17 +0400

    Css changes

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -18,3 +18,24 @@ $("#dangerBtn").on('click', function() {
 $("#tipsBtn").on('click', function() {
   $("#locationSubmit").attr("action", "/tips");
 });
+
+
+
+$(document).ready(function() {
+	var currentBoxNumber = 0;
+	$(".form-control").keydown(function (event) {
+		console.log(event)
+	    if (event.keyCode == 13) {
+	        textboxes = $(".form-control");
+	        currentBoxNumber = textboxes.index(this);
+	        console.log(textboxes, textboxes.index(this));
+	        if (textboxes[currentBoxNumber + 1] != null) {
+	            nextBox = textboxes[currentBoxNumber + 1];
+	            nextBox.focus();
+	            nextBox.select();
+	            event.preventDefault();
+	            return false;
+	        }
+	    }
+	});
+});
--- server.js
+++ server.js
@@ -9,16 +9,19 @@ var express          = require( 'express' )
   , RedisStore       = require( 'connect-redis' )( session )
   , GoogleStrategy   = require( 'passport-google-oauth2' ).Strategy
   , pug              = require( 'pug')
-  , favicon          = require( 'serve-favicon');
-  , port             = process.env.PORT;
+  , mongoose         = require('mongoose')
+  //, favicon          = require( 'serve-favicon')
+  , port             = process.env.PORT || 8002;
 
 https = require('https');
-
+/////////////////////////////////////////////////////////////
+//Configuration
+/////////////////////////////////////////////////////////////
 // configure Express
 app.use(express.static('public'));
 app.set("view engine", "pug");
 app.set("views", "public/views");
-app.use(favicon('public','favicon.ico'));
+//app.use(favicon('public','favicon.ico'));
 
 app.use( cookieParser()); 
 app.use( bodyParser.json());
@@ -30,12 +33,44 @@ app.listen(port, function() {
   console.log('Server started on port ', port);
 });
 
+//Set up default mongoose connection
+ var mongoDB = 
+    process.env.MONGOLAB_URI ||
+    process.env.MONGODB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/my_database';
 
-//Twillio setup
+mongoose.connect(mongoDB, {useMongoClient: true}, function(err){
+    if (err) {
+      console.log ('ERROR connecting' + err);
+    } else {
+      console.log ('Succeeded connected');
+    }
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+/////////////////////////////////////////////////////////////
+//Twillio Text messaging
+/////////////////////////////////////////////////////////////
 var accountSid = 'AC0b4dd92564225048f717aaa016bab864'; 
 var authToken = '36dd70b5af39ea604ed0e883a7a2df9d'; 
 var client = require('twilio')(accountSid, authToken);
 var recipient = '+971563052997';
+
+function send_text(message){
+  // client.messages.create({ 
+  //     to: recipient, 
+  //     from: "+16093725592", 
+  //     body: message, 
+  // }, function(err, messageres) { 
+  //   if (err){
+  //     console.log(err)
+  //   }
+  //     console.log('SMS sent', messageres.sid); 
+  // });
+}
 
 /////////////////////////////////////////////////////////////
 // Google Login API stuff
@@ -105,14 +140,9 @@ app.get('/auth/google/callback',function(req, res, next) {
     if (!user) { return res.redirect('/login'); }
     req.logIn(user, function(err) {
       if (err) { return next(err); }
-      //if it's not an nyu.edu email, go back to login
-      if ((!user.email.endsWith("@nyu.edu"))) { console.log('not nyu', user.email); return res.redirect('/login'); }
       req.session.save(() => {
         setTimeout(function(){
-          res.redirect('/home');
-
-          console.log('saving');  
-          console.log(user);
+          lookup_db(user.email, res)
         }, 1000)        
         return;
       })      
@@ -132,7 +162,128 @@ function ensureAuthenticated(req, res, next) {
   res.redirect('/login');
 }
 
+///////////////////////////////////////////////////////////////////////
+//Database interaction
+///////////////////////////////////////////////////////////////////////
+var DB = {}
 
+function lookup_db(email, res){
+  //check if nyu email
+  if ((!email.endsWith("@nyu.edu"))){
+    console.log('not nyu', email); 
+    return res.redirect('/login'); 
+  }
+  //check if first visit
+  usermodel.findOne({myemail: email}, function(err, found_user){
+    if (err) {
+        console.log("The error while accessing the colleciton is " + err);
+    }
+    if (found_user){
+        res.redirect('/home');
+        console.log('new session with returning user: ', found_user);
+    } else {
+      res.redirect('/tutorial');
+    }
+  })
+}  
+
+var Schema = mongoose.Schema;
+var userschema = new Schema({
+    myemail: String,
+    myphone: String,
+    name1: String,
+    name2: String,
+    name3: String,
+    phone1: String,
+    phone2: String,
+    phone3: String,
+    netid1: String,
+    netid2: String,
+    netid3: String,
+});
+var usermodel = mongoose.model('usermodel', userschema );
+
+function add_or_update(req){
+  usermodel.findOne({myemail: req.session.passport.user.email}, function(err, found_user){
+    if (err) {
+        console.log("The error while updating colleciton is " + err);
+    }
+    if (found_user){
+        console.log('User already exists, info updated')
+        found_user.myphone = req.body.myphone
+        found_user.name1 = req.body.name2
+        found_user.name2 = req.body.name3
+        found_user.name3 = req.body.name3
+        found_user.phone1 = req.body.phone1
+        found_user.phone2 = req.body.phone2
+        found_user.phone3 = req.body.phone3
+        found_user.netid1 = req.body.netid1
+        found_user.netid2 = req.body.netid2
+        found_user.netid3 = req.body.netid3
+        found_user.save(function(err) {
+            if (err)
+                throw err
+            return (null, found_user)
+        });
+    }
+    if (!found_user) {
+      console.log('Saving new user', req.body, req.session.passport.user.email)
+      var newuser = new usermodel({
+        myemail: req.session.passport.user.email,
+        myphone: req.body.myphone,
+        name1: req.body.name2,
+        name2: req.body.name3,
+        name3: req.body.name3,
+        phone1: req.body.phone1,
+        phone2: req.body.phone2,
+        phone3: req.body.phone3,
+        netid1: req.body.netid1,
+        netid2: req.body.netid2,
+        netid3: req.body.netid3,
+      });
+      newuser.save(function (err, fluffy) {
+        if (err) return console.error(err);
+      });
+    }
+  })
+}
+
+
+var recordschema = new Schema({
+    email: String,
+    situ: String,
+    who: String,
+    what: String,
+    when: String
+});
+var recordmodel = mongoose.model('recordmodel', recordschema );
+function add_record(req, sit){
+  console.log(req)
+  var dt = new Date();
+  var newrecord = new recordmodel({
+    email: req.session.passport.user.email,
+    situ: sit,
+    who: req.body.who,
+    what: req.body.what,
+    when: dt.toUTCString()
+  });
+  newrecord.save(function (err, fluffy) {
+    if (err) return console.error(err);
+  });
+}
+
+
+function print_db(){
+  // PRINTS ALL EMTRIES
+  usermodel.find(function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+  })
+  recordmodel.find(function (err, kittens) {
+    if (err) return console.error(err);
+    console.log(kittens);
+  })
+}
 
 ///////////////////////////////////////////////////////////////////////
 //Routes, MAKE SURE TO AUTHENTICATE
@@ -141,52 +292,43 @@ app.get('/home', ensureAuthenticated, function(req, res, err) {
   res.render('home.pug');
 });
 
-
 app.get('/emergency', ensureAuthenticated, function(req, res, err) {
   res.render('emergency.pug', {'myLocation': req.query.myLocation});
-  
-  client.messages.create({ 
-      to: recipient, 
-      from: "+16093725592", 
-      body: req.query.myLocation + " is the location.", 
-  }, function(err, message) { 
-    if (err){
-      console.log(err)
-    }
-      console.log('SMS sent', message.sid); 
-  });
-  
+  send_text(req.query.myLocation)  
 });
 
 app.get('/danger', ensureAuthenticated, function(req, res, err) {
   res.render('danger.pug', {'myLocation': req.query.myLocation});
-  
-  client.messages.create({ 
-      to: recipient, 
-      from: "+16093725592", 
-      body: req.query.location + " is the location.", 
-  }, function(err, message) { 
-    if (err){
-      console.log(err)
-    }
-      console.log('SMS sent', message.sid); 
-  });
-  
+  send_text(req.query.myLocation)
 });
 
+app.get('/tutorial', ensureAuthenticated, function(req, res, err) {
+  console.log(req)
+  res.render('tutorial.pug')
+});
 
 app.get('/settings', ensureAuthenticated, function(req, res, err) {
   res.render('settings.pug')
 });
 
+app.post('/settings/saved', ensureAuthenticated, function(req, res, err) {
+  add_or_update(req)
+  print_db()
+  res.redirect('/home')
+});
+
 app.post('/emergency/submit', ensureAuthenticated, function(req, res, err) {
   console.log(req.body);
   res.render('emergency-submit.pug');
+  add_record(req, 'emergency');
+  send_text(req.body.what); 
 });
 
 app.post('/danger/submit', ensureAuthenticated, function(req, res, err) {
   console.log(req.body);
   res.render('danger-submit.pug');
+  add_record(req, 'danger');
+  send_text(req.body.what);  
 });
 
 app.get('/tips', ensureAuthenticated, function(req, res, err) {
